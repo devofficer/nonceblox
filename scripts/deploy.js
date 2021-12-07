fe56d1359391fc7e293cb1a56c05c6b905260f3e
Nikolay Toplev <nikolaytoplev@gmail.com>
2021-12-07 22:57:35 +0300

    First complete

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,53 +1,21 @@
-// This is a script for deploying your contracts. You can adapt it to deploy
-// yours, or create new ones.
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
+  if (network.name === "rinkeby") {
     console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+      "You are trying to deploy a contract to the Rinkeby Network"
     );
   }
 
-  // ethers is avaialble in the global scope
   const [deployer] = await ethers.getSigners();
   console.log(
     "Deploying the contracts with the account:",
     await deployer.getAddress()
   );
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+  const Lottery = await ethers.getContractFactory("Lottery");
+  const lottery = await Lottery.deploy();
+  await lottery.deployed();
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
-
-  console.log("Token address:", token.address);
-
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
-}
-
-function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = __dirname + "/../frontend/src/contracts";
-
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
-
-  fs.writeFileSync(
-    contractsDir + "/contract-address.json",
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
-
-  const TokenArtifact = artifacts.readArtifactSync("Token");
-
-  fs.writeFileSync(
-    contractsDir + "/Token.json",
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+  console.log("Lottery address:", lottery.address);
 }
 
 main()
--- test/Token.js
+++ test/Token.js
@@ -1,134 +1,84 @@
-// This is an exmaple test file. Hardhat will run every *.js file in `test/`,
-// so feel free to add new ones.
 
-// Hardhat tests are normally written with Mocha and Chai.
-
-// We import Chai to use its asserting functions here.
 const { expect } = require("chai");
+var BigNumber = require('big-number');
 
-// `describe` is a Mocha function that allows you to organize your tests. It's
-// not actually needed, but having your tests organized makes debugging them
-// easier. All Mocha functions are available in the global scope.
+describe("Lottery contract", function () {
 
-// `describe` recieves the name of a section of your test suite, and a callback.
-// The callback must define the tests of that section. This callback can't be
-// an async function.
-describe("Token contract", function () {
-  // Mocha has four functions that let you hook into the the test runner's
-  // lifecyle. These are: `before`, `beforeEach`, `after`, `afterEach`.
-
-  // They're very useful to setup the environment for tests, and to clean it
-  // up after they run.
-
-  // A common pattern is to declare some variables, and assign them in the
-  // `before` and `beforeEach` callbacks.
-
-  let Token;
-  let hardhatToken;
+  let Lottery;
+  let hardhatLottery;
   let owner;
   let addr1;
   let addr2;
   let addrs;
 
-  // `beforeEach` will run before each test, re-deploying the contract every
-  // time. It receives a callback, which can be async.
   beforeEach(async function () {
-    // Get the ContractFactory and Signers here.
-    Token = await ethers.getContractFactory("Token");
+    Lottery = await ethers.getContractFactory("Loterry");
     [owner, addr1, addr2, ...addrs] = await ethers.getSigners();
 
-    // To deploy our contract, we just have to call Token.deploy() and await
-    // for it to be deployed(), which happens onces its transaction has been
-    // mined.
-    hardhatToken = await Token.deploy();
-    await hardhatToken.deployed();
-
-    // We can interact with the contract by calling `hardhatToken.method()`
-    await hardhatToken.deployed();
+    hardhatLottery = await Lottery.deploy();
+    await hardhatLottery.deployed();
   });
 
-  // You can nest describe calls to create subsections.
   describe("Deployment", function () {
-    // `it` is another Mocha function. This is the one you use to define your
-    // tests. It receives the test name, and a callback function.
-
-    // If the callback function is async, Mocha will `await` it.
     it("Should set the right owner", async function () {
-      // Expect receives a value, and wraps it in an assertion objet. These
-      // objects have a lot of utility methods to assert values.
-
-      // This test expects the owner variable stored in the contract to be equal
-      // to our Signer's owner.
-      expect(await hardhatToken.owner()).to.equal(owner.address);
-    });
-
-    it("Should assign the total supply of tokens to the owner", async function () {
-      const ownerBalance = await hardhatToken.balanceOf(owner.address);
-      expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
+      expect(await hardhatLottery.owner()).to.equal(owner.address);
     });
   });
 
-  describe("Transactions", function () {
-    it("Should transfer tokens between accounts", async function () {
-      // Transfer 50 tokens from owner to addr1
-      await hardhatToken.transfer(addr1.address, 50);
-      const addr1Balance = await hardhatToken.balanceOf(
-        addr1.address
-      );
-      expect(addr1Balance).to.equal(50);
-
-      // Transfer 50 tokens from addr1 to addr2
-      // We use .connect(signer) to send a transaction from another account
-      await hardhatToken.connect(addr1).transfer(addr2.address, 50);
-      const addr2Balance = await hardhatToken.balanceOf(
-        addr2.address
-      );
-      expect(addr2Balance).to.equal(50);
-    });
-
-    it("Should fail if sender doesn’t have enough tokens", async function () {
-      const initialOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-
-      // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens).
-      // `require` will evaluate false and revert the transaction.
+  describe("User actions", function () {
+    it("Should be reverted when trying to join before the period", async function () {
       await expect(
-        hardhatToken.connect(addr1).transfer(owner.address, 1)
-      ).to.be.revertedWith("Not enough tokens");
-
-      // Owner balance shouldn't have changed.
-      expect(await hardhatToken.balanceOf(owner.address)).to.equal(
-        initialOwnerBalance
-      );
+        hardhatLottery.connect(addr1).join()
+      ).to.be.revertedWith("Lottery is not started yet");
     });
 
-    it("Should update balances after transfers", async function () {
-      const initialOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
+    it("Should transfer exactly 0.1 Eth to Loterry contract when joining", async function () {
+      await hardhatLottery.connect(owner).startLottery();
+      const prior_balance = BigNumber(addr1.balance);
+      await hardhatLottery.connect(addr1).join();
+      expect(BigNumber(addr1.balance) - prior_balance).to.equal(BigNumber(100000000000000000));
+    });
 
-      // Transfer 100 tokens from owner to addr1.
-      await hardhatToken.transfer(addr1.address, 100);
+    it("Should be reverted when trying to start lottery from non-owner address", async function () {
+      await expect(
+        hardhatLottery.connect(addr1).startLottery()
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    })
+  });
 
-      // Transfer another 50 tokens from owner to addr2.
-      await hardhatToken.transfer(addr2.address, 50);
+  describe("Owner actions", function () {
+    it("Should change the set lottery status as true after start", async function () {
+      expect(hardhatLottery.lotteryStarted()).to.be.false;
+      await hardhatLottery.connect(owner).startLottery();
+      expect(hardhatLottery.lotteryStarted()).to.be.true;
+    });
 
-      // Check balances.
-      const finalOwnerBalance = await hardhatToken.balanceOf(
-        owner.address
-      );
-      expect(finalOwnerBalance).to.equal(initialOwnerBalance - 150);
+    it("Should be reverted when trying start lottery again", async function () {
+      await(
+        hardhatLottery.connect(owner).startLottery()
+      ).to.be.revertedWith("Lottery is already started");
+    });
 
-      const addr1Balance = await hardhatToken.balanceOf(
-        addr1.address
-      );
-      expect(addr1Balance).to.equal(100);
+    it("Should be reverted when trying to pick before period", async function () {
+      await expect(
+        hardhatLottery.connect(owner).pickWinner()
+      ).to.be.revertedWith("Lottery is still ongoing");
+    });
 
-      const addr2Balance = await hardhatToken.balanceOf(
-        addr2.address
-      );
-      expect(addr2Balance).to.equal(50);
+    it("Should return random id within the range of users", async function () {
+      await hardhatLottery.connect(owner).join();
+      await hardhatLottery.connect(addr1).join();
+      await hardhatLottery.connect(addr2).join();
+      await hardhatLottery.connect(addrs).join();
+      await hre.network.provider.request({
+        method: "evm_increaseTime",
+        params:["3600"]
+      });
+      await hardhatLottery.connect(owner).pickWinner();
+      const result = await hardhatLottery.randomResult();
+      const bound = await hardhatLottery.users().length;
+      expect(result).to.be.above(0);
+      expect(result).to.be.most(bound);
     });
   });
 });
